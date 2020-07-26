2f3e11ec90fd9729bb7c9c7fb8c483a7b3ac42cd
Juan Francisco <contrerasjf.0@hotmail.com>
2020-07-26 10:23:51 -0500

    :sparkles: feature/refactor(courseQuery): Implement a mutation to save one couser and the structure was changed(Mutation and Inputs)
    * All files related with GraphQL were moved to the /lib/graphql diretory.
    * A file for mutations was created to implement all mutation.
    * A file for queries was created and the queries on the resolvers file was changed to queries file.
    * The createCourse mutation and CourseInput input were created on the schema file.
    
    Resolve: C-10

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
 
-const resolvers = require('./lib/resolvers')
+const resolvers = require('./lib/graphql/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
 // Define the schema
 const typeDefs = readFileSync(
-  join(__dirname, 'lib', 'schema.graphql'),
+  join(__dirname, 'lib', 'graphql', 'schema.graphql'),
   'utf-8'
 )
 const schema = makeExecutableSchema({ typeDefs, resolvers })
