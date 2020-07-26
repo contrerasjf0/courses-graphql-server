203dfe7ad8cf935b53f171ad2d8d84bed63eadcc
Juan Francisco <contrerasjf.0@hotmail.com>
2020-07-25 22:12:10 -0500

    :recycle: refactor(schemas): Implement the getCourses query and Custom types was added
    * The way to define the schemas was changed on the index.js file.
    * The resolvers file was created to implement the resolvers for the queries.
    * A file(schemas.graphql) file was created to add the schemas.
    * The standard library was installed, and new scripts were added with standard.
    
    Resolve: C-07

--- index.js
+++ index.js
@@ -3,29 +3,25 @@
 const express = require('express')
 const { buildSchema } = require('graphql')
 const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3000
 
-//Define the schema
-const schema = buildSchema(`
-  type Query {
-    "Retorna un saludo al mundo"
-    hello: String
-  }
-`)
-
-// Configure the resolvers
-const resolvers = {
-  hello: () => {
-    return 'Hola Mundo'
-  }
-}
+// Define the schema
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8')
+)
 
 app.use('/api', gqlMiddleware({
   schema: schema,
   rootValue: resolvers,
-  graphiql: true //Work in development mode
+  graphiql: true // Work in development mode
 }))
 
 app.listen(port, () => {
