9d4a92e3321370a721618e930d727334720cdea2
Juan Francisco <contrerasjf.0@hotmail.com>
2020-07-26 08:07:19 -0500

    :sparkles: feature(coursesQuery): Implement a new query (Argument on queries)
    * A new query(getCourse)  was defined on the schema to retrieve one course.
    * The getCourse method was defined on the resolvers file for the query getCourse.
    * The library graphql-tools was installed.
    * The graphql's buildSchema method was remplaced by graphql-tools's makeExecutableSchema method.
    
    Resolve: C-07

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const express = require('express')
-const { buildSchema } = require('graphql')
+
+const { makeExecutableSchema } = require('graphql-tools')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
@@ -12,11 +13,11 @@ const app = express()
 const port = process.env.port || 3000
 
 // Define the schema
-const schema = buildSchema(
-  readFileSync(
-    join(__dirname, 'lib', 'schema.graphql'),
-    'utf-8')
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
 )
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 app.use('/api', gqlMiddleware({
   schema: schema,
